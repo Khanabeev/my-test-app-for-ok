e29141978fb84cbad616d04ef64cb8424eeae3d4
Anton Khanabeev <khanabeev@gmail.com>
2025-01-13 21:06:48 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-test-app-for-ok"
+  base: "/my-test-app-for-ok",
+  build: {
+    outDir: 'build', // Указываем директорию для сборки
+  },
 })
